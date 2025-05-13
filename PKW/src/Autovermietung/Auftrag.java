7e0e32ee55b6b4d12f3e09d9c08c5d81d12cc9eb
ErbseOhneSuppe <gt977yt@gmail.com>
2025-05-13 13:21:47 +0200

    Update Auftrag.java

--- PKW/src/Autovermietung/Auftrag.java
+++ PKW/src/Autovermietung/Auftrag.java
@@ -1,83 +1,94 @@
-package Autovermietung;
+package PKWVermietungsverwaltung;
 
 public class Auftrag {
-    private String Familienname;
-    private String AnfangsDatum;
-    private String EndDatum;
-    private String Tagespreis;
-    private String ExtraPaket;
-    private String Fahrzeug;
-    private String Seriennummer;
+	
+	private String ID;
+	private String AnfangsDatum;
+	private String EndDatum;
+	private String FahrzeugID;
+	private String KundenID;
+	private String ExtraID;
+	private String Gesamtpreis;
 
-    public Auftrag(String Familienname, String AnfangsDatum, String EndDatum, String Tagespreis, String ExtraPaket,
-                   String Fahrzeug, String Seriennummer) {
-        this.Familienname = Familienname;
-        this.AnfangsDatum = AnfangsDatum;
-        this.EndDatum = EndDatum;
-        this.Tagespreis = Tagespreis;
-        this.ExtraPaket = ExtraPaket;
-        this.Fahrzeug = Fahrzeug;
-        this.Seriennummer = Seriennummer;
-    }
+	public Auftrag(String ID, String AnfangsDatum, String EndDatum,  String FahrzeugID, String KundenID, String ExtraID,  String Gesamtpreis) {
+		this.ID = ID;
+		this.AnfangsDatum = AnfangsDatum;
+		this.EndDatum = EndDatum;
+		this.FahrzeugID = FahrzeugID;
+		this.KundenID = KundenID;
+		this.ExtraID = ExtraID;
+		this.Gesamtpreis = Gesamtpreis;
+	}
 
-    public void display() {
-        System.out.println("-----===== Informationen Auftrag =====-----");
-        System.out.println("Familienname: " + Familienname);
-        System.out.println("Start Datum: " + AnfangsDatum);
-        System.out.println("End Datum: " + EndDatum);
-        System.out.println("Tagespreis: " + Tagespreis);
-        System.out.println("ExtraPaket: " + ExtraPaket);
-        System.out.println("Fahrzeug: " + Fahrzeug);
-        System.out.println("Fahrzeug Seriennummer: " + Seriennummer);
-        System.out.println("-----=================================-----");
-        System.out.println("");
-    }
+	public void display() {
+		System.out.println("-----===== Informationen Auftrag =====-----");
+		System.out.println("ID: " + ID);
+		System.out.println("Start Datum: " + AnfangsDatum);
+		System.out.println("End Datum: " + EndDatum);
+		System.out.println("FahrzeugID: " + FahrzeugID);
+		System.out.println("KundenID: " + KundenID);
+		System.out.println("ExtraID: " + ExtraID);
+		System.out.println("Gesamtpreis: " + Gesamtpreis);
+		System.out.println("-----=================================-----");
+		System.out.println("");
+	}
 
-    public String getAnfangsDatum() {
-        return AnfangsDatum;
-    }
+	public String getID() {
+		return ID;
+	}
 
-    public void setAnfangsDatum(String anfangsDatum) {
-        AnfangsDatum = anfangsDatum;
-    }
+	public void setID(String iD) {
+		ID = iD;
+	}
 
-    public String getEndDatum() {
-        return EndDatum;
-    }
+	public String getAnfangsDatum() {
+		return AnfangsDatum;
+	}
 
-    public void setEndDatum(String endDatum) {
-        EndDatum = endDatum;
-    }
+	public void setAnfangsDatum(String anfangsDatum) {
+		AnfangsDatum = anfangsDatum;
+	}
 
-    public String getTagespreis() {
-        return Tagespreis;
-    }
+	public String getEndDatum() {
+		return EndDatum;
+	}
 
-    public void setTagespreis(String tagespreis) {
-        Tagespreis = tagespreis;
-    }
+	public void setEndDatum(String endDatum) {
+		EndDatum = endDatum;
+	}
 
-    public String getExtraPaket() {
-        return ExtraPaket;
-    }
+	public String getFahrzeugID() {
+		return FahrzeugID;
+	}
 
-    public void setExtraPaket(String extraPaket) {
-        ExtraPaket = extraPaket;
-    }
+	public void setFahrzeugID(String fahrzeugID) {
+		FahrzeugID = fahrzeugID;
+	}
 
-    public String getFahrzeug() {
-        return Fahrzeug;
-    }
+	public String getKundenID() {
+		return KundenID;
+	}
 
-    public void setFahrzeug(String fahrzeug) {
-        Fahrzeug = fahrzeug;
-    }
+	public void setKundenID(String kundenID) {
+		KundenID = kundenID;
+	}
 
-    public String getSeriennummer() {
-        return Seriennummer;
-    }
+	public String getExtraID() {
+		return ExtraID;
+	}
 
-    public void setSeriennummer(String seriennummer) {
-        Seriennummer = seriennummer;
-    }
+	public void setExtraID(String extraID) {
+		ExtraID = extraID;
+	}
+
+	public String getGesamtpreis() {
+		return Gesamtpreis;
+	}
+
+	public void setGesamtpreis(String gesamtpreis) {
+		Gesamtpreis = gesamtpreis;
+	}
+	
+	
+
 }
